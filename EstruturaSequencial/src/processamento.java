49c65d55449ed39f953f7333e8c020a96a8794ba
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-01-30 18:16:27 -0300

    Tipos de dados em Java

--- EstruturaSequencial/src/processamento.java
+++ EstruturaSequencial/src/processamento.java
@@ -1,14 +1,37 @@
+import java.awt.image.RescaleOp;
 
 public class processamento {
 
 	public static void main(String[] args) {
 
-		int x, y;
-		x = 5;
-		y = 2 * x;
+		// int x;
+		// double y;
+		// x = 5;
+		// y = 2 * x;
+
+		// double b, B, h, area;
+
+		// b = 6.0;
+		// B = 8.0;
+		// = 5.0;
+		// area = (b + B) / 2.0 * h;
+
+		//int a, b;
+		// resultado;
+
+		//a = 5;
+		//b = 2;
+
+		//resultado = (double) a / b;
 		
-		System.out.println(x);
-		System.out.println(y);
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		// System.out.println(x);
+		System.out.println(b);
 
 	}
 
